7f8952b66a63d922b7c8c1103a670c6cbba083e0
Bjoernwenn <desmodue270@gmail.com>
2026-02-06 15:01:19 +0100

    Update Analyse.R

--- Analyse.R
+++ Analyse.R
@@ -1,6 +1,18 @@
 daten <- read.csv("daten.csv", na.strings = "")
 source("funktionen.R")
 
+daten$Age_class <- cut(
+  daten$Age,
+  breaks = c(0,12,21,50,100),
+  labels = c("Kinder", "Jugendliche","Erwachsene","Ältere"),
+)
+
+daten$Fare_class <- cut(
+  daten$Fare,
+  breaks = c(0,30,100,513),
+  labels = c("low", "medium","high")
+)
+
 desk_metrisch(daten, "Age")
 
 desk_metrisch(daten, "Fare")
@@ -20,6 +32,12 @@ desk_kategorial(daten, "Anrede")
 desk_kategorial(daten, "Bord") #78% NA nicht geeignet für Analyse
 desk_kategorial(daten, "Deck") #77% NA auch nicht geeignet
 desk_kategorial(daten, "SibSp")
+desk_kategorial(daten, "Agegroup")
+desk_kategorial(daten, "Fare_class")
+desk_kategorial(daten, "Age_class")
+
+
+
 
 bivariat_kat(daten, "Survived", "Pclass") #4.549252e-23 
 bivariat_kat(daten, "Survived", "Sex") #1.197357e-58
@@ -36,6 +54,7 @@ bivariat_kat(daten, "Sex", "Embarked") #0.001258525
 bivariat_kat(daten, "Age", "Pclass") #3.695126e-13
 bivariat_kat(daten, "Age", "Embarked") #1.726494e-08
 bivariat_kat(daten, "Embarked","Pclass") #8.435268e-26
+bivariat_kat(daten, "Pclass", "Fare_class")
              
              
              
@@ -43,11 +62,14 @@ bivariat_kat(daten, "Embarked","Pclass") #8.435268e-26
 bivariat_metr_dichotom(daten, "Age", "Survived")
 bivariat_metr_dichotom(daten, "Age", "Sex")
 bivariat_metr_dichotom(daten, "Age", "Embarked")
-#Age und Fare hat zu viele Einträge sehr unübersichtlich
+bivariat_metr_dichotom(daten, "Age", "Preisklasse")
 bivariat_metr_dichotom(daten, "Age", "Pclass")
 bivariat_metr_dichotom(daten, "Fare", "Embarked")
 bivariat_metr_dichotom(daten, "Fare", "Survived")
 bivariat_metr_dichotom(daten, "Fare", "Sex")
 bivariat_metr_dichotom(daten, "Fare", "Pclass")
+bivariat
 
 plot_multi(daten, "Sex","Survived", "Pclass")
+plot_multi(daten, "Pclass", "Survived", "Agegroup")
+plot_multi(daten, "Pclass", "Preisklasse","Agegroup")
